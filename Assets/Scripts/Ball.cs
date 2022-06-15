053a5a514a2d5fa33db2976a254a7bffc52f4816
Andrew Gvozdev <angvoz.dev@gmail.com>
2022-06-15 11:57:39 -0400

    Initial revision - spawns balls in random on 9x9 board

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    static private int ballNumber = 0;
+
+    public Animator animator;
+    public bool isSelected = false;
+
+    public const float BALL_LEVEL = -1.0f;
+
+    static private Ball create(GameObject prefab, float scale, Board board, int x, int y) {
+        GameObject ballObject = Instantiate(prefab, board.cellToCamera(x, y, BALL_LEVEL), Quaternion.identity, /* parent */ board.transform);
+
+        Ball ball = ballObject.GetComponent<Ball>();
+        ball.name = "Ball " + ++ballNumber + " [" + x + ", " + y + " ]";
+
+        Renderer renderer = ballObject.GetComponent<Renderer>();
+        renderer.material.color = Color.green;
+        renderer.transform.localScale = new Vector3(scale, scale, scale);
+        return ball;
+    }
+
+    static private Ball spawn(GameObject prefab, float scale, Board board) {
+        int x = Random.Range(0, board.boardDimension);
+        int y = Random.Range(0, board.boardDimension);
+
+        Ball ball = create(prefab, scale, board, x, y);
+        return ball;
+    }
+
+    static public List<Ball> spawn(int n, GameObject prefab, float scale, Board board) {
+        List<Ball> balls = new List<Ball>();
+        for (int i = 0; i < n; i++) {
+            Ball b = Ball.spawn(prefab, scale, board);
+            if (b != null) {
+                balls.Add(b);
+            }
+        }
+        return balls;
+    }
+
+    static public void UnselectAll() {
+        foreach (Ball ball in GameObject.FindObjectsOfType<Ball>()) {
+            ball.Select(false);
+        }
+    }
+
+    public void Select(bool select = true) {
+        isSelected = select;
+        if (animator != null) {
+            animator.SetBool("selected", isSelected);
+        }
+
+    }
+
+    private void OnMouseUp() {
+        bool select = !isSelected;
+        if (select) {
+            UnselectAll();
+        }
+        Select(select);
+    }
+}
