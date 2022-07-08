7e847bfe073a89f1e84582320098b4eade367ac8
Andrew Gvozdev <angvoz.dev@gmail.com>
2022-07-08 18:36:26 -0400

    Add 7 ball colors

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
+    private const int NUMBER_OF_COLORS = 7;
+    
+    public int color = 0;
     static private int ballNumber = 0;
 
     public Animator animator;
@@ -13,14 +16,43 @@ public class Ball : MonoBehaviour {
 
     public const float BALL_LEVEL = -1.0f;
 
+    private readonly struct NamedColor {
+        public readonly Color color;
+        public readonly string name;
+
+        public NamedColor(Color c, string n) {
+            this.color = c;
+            this.name = n;
+        }
+
+        public NamedColor(int r, int g, int b, string n) {
+            this.color = new Color(r / 255f, g / 255f, b / 255f);
+            this.name = n;
+        }
+    }
+
     static private Ball create(GameObject prefab, float scale, Board board, int x, int y) {
+        NamedColor[] colors = {
+            new NamedColor(255, 51, 51, "Red"),
+            new NamedColor(Color.cyan, "Cyan"),
+            new NamedColor(255, 0, 255, "Purple"),
+            new NamedColor(30, 144, 255, "Blue"),
+            new NamedColor(Color.green, "Green"),
+            new NamedColor(230, 128, 0, "Brown"),
+            new NamedColor(Color.yellow, "Yellow"),
+            new NamedColor(Color.black, "Black"),
+        };
+        int numberOfColors = NUMBER_OF_COLORS <= colors.Length ? NUMBER_OF_COLORS : colors.Length;
+        int colorIndex = Random.Range(0, numberOfColors);
+        NamedColor color = colors[colorIndex];
         GameObject ballObject = Instantiate(prefab, board.cellToCamera(x, y, BALL_LEVEL), Quaternion.identity, /* parent */ board.transform);
 
         Ball ball = ballObject.GetComponent<Ball>();
-        ball.name = "Ball " + ++ballNumber + " [" + x + ", " + y + " ]";
+        ball.color = colorIndex;
+        ball.name = color.name + " Ball " + ++ballNumber + " [" + x + ", " + y + " ]";
 
         Renderer renderer = ballObject.GetComponent<Renderer>();
-        renderer.material.color = Color.green;
+        renderer.material.color = color.color;
         renderer.transform.localScale = new Vector3(scale, scale, scale);
         return ball;
     }
